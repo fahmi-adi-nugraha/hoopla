88f98344212d3394781bd3345e879aae43161063
Fahmi Adi Nugraha <fnugraha@gmail.com>
2025-10-28 21:41:55 +0700

    Add functionality for exact keyword match

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 
 import argparse
+import json
+from pathlib import Path
+from typing import Any
+
+MOVIES_FILE_PATH = Path("data/movies.json")
+
+
+def get_movies_matching_keywords(movie_data_path: Path, keywords: str) -> list[str]:
+    with open(movie_data_path, "r") as movie_data_file:
+        movie_data: dict[str, list[dict[str, Any]]] = json.load(movie_data_file)
+    movie_matches: list[dict[str, Any]] = []
+    for movie in movie_data["movies"]:
+        if keywords in movie["title"]:
+            movie_matches.append(movie)
+    return movie_matches
 
 
 def main() -> None:
@@ -15,6 +30,9 @@ def main() -> None:
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
+            movies = get_movies_matching_keywords(MOVIES_FILE_PATH, args.query)
+            for i, movie in enumerate(sorted(movies, key=lambda m: m["id"])[:5]):
+                print(f"{i + 1}. {movie['title']}")
         case _:
             parser.print_help()
 
