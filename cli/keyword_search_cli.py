25a071edf064e74fbfceb866c5b8767157ef892f
Fahmi Adi Nugraha <fnugraha@gmail.com>
2025-10-31 09:15:30 +0700

    Add stop word removal to the text processing pipeline

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -9,6 +9,9 @@ from text_processing.text_processing import clean_text
 
 MOVIES_FILE_PATH = Path("data/movies.json")
 
+# Hardcode until we know for sure we'll ask user for location
+STOPWORDS_FILE = Path("data/stopwords.txt")
+
 
 def match_movie_title(keyword_tokens: list[str], movie_title_tokens: list[str]) -> bool:
     for movie_title_tok in movie_title_tokens:
@@ -18,13 +21,22 @@ def match_movie_title(keyword_tokens: list[str], movie_title_tokens: list[str]) 
     return False
 
 
-def get_movies_matching_keywords(movie_data_path: Path, keywords: str) -> list[str]:
+def get_stopwords(stop_words_file: Path) -> list[str]:
+    stop_words: list[str]
+    with open(stop_words_file, "r") as swf:
+        stop_words = swf.read().splitlines()
+    return stop_words
+
+
+def get_movies_matching_keywords(
+    movie_data_path: Path, keywords: str, stop_words: list[str]
+) -> list[str]:
     with open(movie_data_path, "r") as movie_data_file:
         movie_data: dict[str, list[dict[str, Any]]] = json.load(movie_data_file)
     movie_matches: list[dict[str, Any]] = []
-    keywords_cleaned = clean_text(keywords)
+    keywords_cleaned = clean_text(keywords, stop_words)
     for movie in movie_data["movies"]:
-        movie_title_cleaned = clean_text(movie["title"])
+        movie_title_cleaned = clean_text(movie["title"], stop_words)
         if match_movie_title(keywords_cleaned, movie_title_cleaned):
             movie_matches.append(movie)
     return movie_matches
@@ -42,7 +54,10 @@ def main() -> None:
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
-            movies = get_movies_matching_keywords(MOVIES_FILE_PATH, args.query)
+            stop_words = get_stopwords(STOPWORDS_FILE)
+            movies = get_movies_matching_keywords(
+                MOVIES_FILE_PATH, args.query, stop_words
+            )
             for i, movie in enumerate(sorted(movies, key=lambda m: m["id"])[:5]):
                 print(f"{i + 1}. {movie['title']}")
         case _:
--- cli/text_processing/text_processing.py
+++ cli/text_processing/text_processing.py
@@ -1,8 +1,12 @@
 import string
-from functools import reduce
+from functools import partial, reduce
 from typing import Callable
 
-TextProcFunc = Callable[[str], str] | Callable[[str], list[str]]
+TextProcFunc = Callable[[str | list[str]], str | list[str]]
+
+
+def remove_stop_words(text: list[str], stop_words: list[str]) -> list[str]:
+    return [word for word in text if word not in stop_words]
 
 
 def tokenize(text: str) -> list[str]:
@@ -19,10 +23,11 @@ def convert_to_lower(text: str) -> str:
     return text.lower()
 
 
-def clean_text(text: str) -> list[str]:
+def clean_text(text: str, stop_words: list[str]) -> list[str]:
     func_list: list[TextProcFunc] = [
         convert_to_lower,
         remove_punctuation,
         tokenize,
+        partial(remove_stop_words, stop_words=stop_words),
     ]
     return reduce(lambda acc, func: func(acc), func_list, text)
