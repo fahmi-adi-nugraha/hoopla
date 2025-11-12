2965f06130d5b47b185529cb826ca3a43ea8b58a
Fahmi Adi Nugraha <fnugraha@gmail.com>
2025-11-12 21:02:08 +0700

    Refactor proc to move the code in each match arm to separate functions

--- cli/keyword_search/general.py
+++ cli/keyword_search/general.py
@@ -2,7 +2,7 @@ import sys
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
-from .inverted_index import InvertedIndex
+from .inverted_index import BM25_K1, InvertedIndex
 from .keyword_search import get_movies_matching_keywords, get_stopwords
 
 MOVIES_FILE_PATH = Path("data/movies.json")
@@ -20,45 +20,69 @@ def load_index(inverted_index: InvertedIndex) -> None:
         sys.exit(1)
 
 
+def search_command(inv_idx: InvertedIndex, query: str) -> None:
+    load_index(inv_idx)
+    print(f"Searching for: {query}")
+    stopwords = get_stopwords(STOPWORDS_FILE)
+    movies = get_movies_matching_keywords(inv_idx, query, stopwords)
+    for i, movie in enumerate(sorted(movies, key=lambda m: m["id"])):
+        print(f"{i + 1}. {movie['title']}")
+
+
+def build_command(inv_idx: InvertedIndex) -> None:
+    stopwords = get_stopwords(STOPWORDS_FILE)
+    inv_idx.build(MOVIES_FILE_PATH, stopwords)
+
+
+def tf_command(inv_idx: InvertedIndex, doc_id: int, term: str) -> None:
+    load_index(inv_idx)
+    tf = inv_idx.get_tf(doc_id, term)
+    print(f"TF score of '{term}' in '{doc_id}': {tf}")
+
+
+def idf_command(inv_idx: InvertedIndex, term: str) -> None:
+    load_index(inv_idx)
+    idf = inv_idx.get_idf(term)
+    print(f"IDF score of '{term}': {idf}")
+
+
+def tfidf_command(inv_idx: InvertedIndex, doc_id: int, term: str) -> None:
+    load_index(inv_idx)
+    tf = inv_idx.get_tf(doc_id, term)
+    idf = inv_idx.get_idf(term)
+    tfidf = tf * idf
+    print(f"TF-IDF score of '{term}' in document '{doc_id}': {tfidf:.2f}")
+
+
+def bm25_tf_command(
+    inv_idx: InvertedIndex, doc_id: int, term: str, k1: float = BM25_K1
+) -> None:
+    load_index(inv_idx)
+    bm25tf = inv_idx.get_bm25_tf(doc_id, term, k1)
+    print(f"BM25 TF score of '{term}' in document '{doc_id}': {bm25tf:.2f}")
+
+
+def bm25_idf_command(inv_idx: InvertedIndex, term: str) -> None:
+    load_index(inv_idx)
+    bm25idf = inv_idx.get_bm25_idf(term)
+    print(f"BM25 IDF score of '{term}': {bm25idf:.2f}")
+
+
 def proc(inv_idx: InvertedIndex, args: Namespace, arg_parser: ArgumentParser) -> None:
     match args.command:
         case "search":
-            load_index(inv_idx)
-            print(f"Searching for: {args.query}")
-            stopwords = get_stopwords(STOPWORDS_FILE)
-            movies = get_movies_matching_keywords(inv_idx, args.query, stopwords)
-            for i, movie in enumerate(sorted(movies, key=lambda m: m["id"])):
-                print(f"{i + 1}. {movie['title']}")
+            search_command(inv_idx, args.query)
         case "build":
-            stopwords = get_stopwords(STOPWORDS_FILE)
-            inv_idx.build(MOVIES_FILE_PATH, stopwords)
+            build_command(inv_idx)
         case "tf":
-            load_index(inv_idx)
-            tf = inv_idx.get_tf(args.tf_doc_id, args.tf_term)
-            doc_title = inv_idx.docmap[args.tf_doc_id]["title"]
-            print(f"Number of times '{args.tf_term}' appears in '{doc_title}': {tf}")
+            tf_command(inv_idx, args.tf_doc_id, args.tf_term)
         case "idf":
-            load_index(inv_idx)
-            idf = inv_idx.get_idf(args.idf_term)
-            print(f"IDF score of '{args.idf_term}': {idf:.2f}")
+            idf_command(inv_idx, args.idf_term)
         case "tfidf":
-            load_index(inv_idx)
-            tf = inv_idx.get_tf(args.tfidf_doc_id, args.tfidf_term)
-            doc_title = inv_idx.docmap[args.tfidf_doc_id]["title"]
-            idf = inv_idx.get_idf(args.tfidf_term)
-            tfidf = tf * idf
-            print(
-                f"TF-IDF score of '{args.tfidf_term}' in document '{doc_title}': {tfidf:.2f}"
-            )
+            tfidf_command(inv_idx, args.tfidf_doc_id, args.tfidf_term)
         case "bm25tf":
-            load_index(inv_idx)
-            bm25tf = inv_idx.get_bm25_tf(args.bm25tf_doc_id, args.bm25tf_term)
-            print(
-                f"BM25 TF score of '{args.bm25tf_term}' in document '{args.bm25tf_doc_id}': {bm25tf:.2f}"
-            )
+            bm25_tf_command(inv_idx, args.bm25tf_doc_id, args.bm25tf_term)
         case "bm25idf":
-            load_index(inv_idx)
-            bm25idf = inv_idx.get_bm25_idf(args.bm25idf_term)
-            print(f"BM25 IDF score of '{args.bm25idf_term}': {bm25idf:.2f}")
+            bm25_idf_command(inv_idx, args.bm25idf_term)
         case _:
             arg_parser.print_help()
